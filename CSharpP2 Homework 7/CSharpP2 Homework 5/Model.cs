a010bb969e120e44a7fea2ae3b417919ab255aec
My3lianOne <my3lian1@gmail.com>
2018-03-15 20:15:09 +0300

    ДЗ7

---
+++ CSharpP2 Homework 7/CSharpP2 Homework 5/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace CSharpP2_Homework_5
+{
+    class Model
+    {
+        ObservableCollection<Employee> employees;
+        ObservableCollection<Department> departments;
+
+
+        public Model()
+        {
+            employees = new ObservableCollection<Employee>();
+            departments = DepartmentBook.Departments;
+        }
+
+        #region Данные
+
+        public ObservableCollection<Employee> Employees { get { return this.employees; } }
+        public ObservableCollection<Department> Departments { get { return this.departments; } }
+        #endregion
+    }
+}
