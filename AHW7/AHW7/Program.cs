09b85540eb17e478b9ec8ddecb959989469d8cc7
itorochkov <itorochkov@bivgroup.com>
2018-10-18 14:52:29 +0300

    Проект для ДЗ7

---
+++ AHW7/AHW7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHW7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
